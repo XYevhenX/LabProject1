e96d2c4293e5b2ef7779a1c8649b2dfe7b0e9e45
Yevhen <evgenzuzin35@gmail.com>
2025-11-09 17:08:21 +0200

    domain checking added

--- LabProject1/LabProject1.cpp
+++ LabProject1/LabProject1.cpp
@@ -19,11 +19,23 @@ double input() {
 	cout << endl;
 	return x;
 }
+bool is_in_domain(double x) {
+	return (x > -9) && (x != -4) && (x != 14);
+}
 
 int main() {	
 	try {
 		greeting();
 		double x = input();
+		cout << "***** do calculations ... ";
+		double result;
+		if (is_in_domain(x)) {
+			//result = f(x);
+		}
+		else {
+			result = NAN;
+		}
+		cout << "done" << endl;
 	}
 	catch (exception& e) {
 		cout << e.what() << endl;
